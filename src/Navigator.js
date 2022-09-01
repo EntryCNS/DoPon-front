173bb31d85e9b4882352db7dbf34e4fcc2fee424
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-09-01 19:23:27 +0900

    plus button

--- src/Navigator.js
+++ src/Navigator.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import TabNavigation from "./navigations/Tab";
 import StackNavigation from "./navigations/Stack";
+
 const Navigator = () => {
   const isLogin = true;
   return (
--- src/components/plan/Plan.jsx
+++ src/components/plan/Plan.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import styled from "styled-components";
 
 const Title = styled.Text`
@@ -43,26 +43,79 @@ const CardPeriod = styled.Text`
   color: #bcbcbc;
 `;
 
+const Plus = styled.View`
+  height: 64px;
+  width: 303px;
+  background: #538ee6;
+  border-radius: 15px;
+  position: fixed;
+  bottom: 0%;
+  left: 31.5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CardContainer = styled.ScrollView`
+  height: 494px;
+`;
+
+const Container = styled.View`
+  height: 636px;
+`;
+
+const PlusText = styled.Text`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 29px;
+  /* identical to box height */
+
+  text-align: center;
+  letter-spacing: -0.02em;
+
+  color: #ffffff;
+`;
+
 const Plan = () => {
   return (
-    <View>
+    <Container>
       <Title>계획</Title>
-      <Card>
-        <CardTitle>가방</CardTitle>
-        <CardPrice>2,085,396</CardPrice>
-        <CardPeriod>2022.03.22 ~ 2022.08.01</CardPeriod>
-      </Card>
-      <Card>
-        <CardTitle>가방</CardTitle>
-        <CardPrice>2,085,396</CardPrice>
-        <CardPeriod>2022.03.22 ~ 2022.08.01</CardPeriod>
-      </Card>
-      <Card>
-        <CardTitle>가방</CardTitle>
-        <CardPrice>2,085,396</CardPrice>
-        <CardPeriod>2022.03.22 ~ 2022.08.01</CardPeriod>
-      </Card>
-    </View>
+      <CardContainer>
+        <Card>
+          <CardTitle>가방</CardTitle>
+          <CardPrice>2,085,396</CardPrice>
+          <CardPeriod>2022.03.22 ~ 2022.08.01</CardPeriod>
+        </Card>
+        <Card>
+          <CardTitle>가방</CardTitle>
+          <CardPrice>2,085,396</CardPrice>
+          <CardPeriod>2022.03.22 ~ 2022.08.01</CardPeriod>
+        </Card>
+        <Card>
+          <CardTitle>가방</CardTitle>
+          <CardPrice>2,085,396</CardPrice>
+          <CardPeriod>2022.03.22 ~ 2022.08.01</CardPeriod>
+        </Card>
+        <Card>
+          <CardTitle>가방</CardTitle>
+          <CardPrice>2,085,396</CardPrice>
+          <CardPeriod>2022.03.22 ~ 2022.08.01</CardPeriod>
+        </Card>
+        <Card>
+          <CardTitle>가방</CardTitle>
+          <CardPrice>2,085,396</CardPrice>
+          <CardPeriod>2022.03.22 ~ 2022.08.01</CardPeriod>
+        </Card>
+        <Card>
+          <CardTitle>가방</CardTitle>
+          <CardPrice>2,085,396</CardPrice>
+          <CardPeriod>2022.03.22 ~ 2022.08.01</CardPeriod>
+        </Card>
+      </CardContainer>
+      <Plus>
+        <PlusText>추가하기</PlusText>
+      </Plus>
+    </Container>
   );
 };
 
