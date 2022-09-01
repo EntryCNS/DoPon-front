50a2f68641a295a0f4483bde28bd2a0523230631
진영 <81845425+baejin1018@users.noreply.github.com>
2022-09-01 20:24:17 +0900

    feat : dropdown

--- src/components/analytics/Consumption/index.js
+++ src/components/analytics/Consumption/index.js
@@ -1,17 +1,18 @@
-import React, { useState } from "react";
-import { Text } from "react-native";
-import { Button } from "react-native";
+import React, { useEffect, useState } from "react";
+
 import styled from "styled-components/native";
 import { VictoryBar, VictoryChart, VictoryAxis } from "victory-native";
 import AntDesign from "react-native-vector-icons/AntDesign";
+import DropDownPicker from "react-native-dropdown-picker";
+const Container = styled.View``;
 
-const Container = styled.View`
+const ChartContainer = styled.View`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 350px;
   height: 275px;
-  border-radius: 20;
+  border-radius: 20px;
   background-color: #fff;
 `;
 
@@ -31,37 +32,79 @@ const data = [
 ];
 
 const Consumption = () => {
+  // useEffect(() => {
+  //   findMonthData();
+  // }, [data]);
+
+  //data에 month 정보만 따로 모아주는 함수
+  // const findMonthData = () => {
+  //   const monthArray = [];
+  //   const amountArray = [];
+  //   data.forEach((d) => {
+  //     monthArray.push(d.month);
+  //     amountArray.push(d.amount);
+  //   });
+  //   setMonthData(monthArray);
+  //   setAmountData(amountArray);
+  // };
+
+  // const [monthData, setMonthData] = useState([]);
+  // const [amountData, setAmountData] = useState([]);
+
+  useEffect(() => {
+    makeItemData();
+  }, [data]);
+
+  const makeItemData = () => {
+    data.forEach((data) => {
+      const itemObject = {
+        label: data.month,
+        value: `${data.month} ${data.amount}`,
+      };
+      itemData.push(itemObject);
+    });
+  };
+
   const date = new Date();
-  const [month, setMonth] = useState(date.getMonth() + 1);
+
+  const [value, setValue] = useState(date.getMonth() + 1);
+  const itemData = [];
+  const [item, setItem] = useState(itemData);
+  const [openDropDown, setOpenDropDown] = useState(false);
+
   return (
     <Container>
-      <MonthConSimption>{month}월의 소비금액은 1111원</MonthConSimption>
-      <AntDesign name="caretdown" />
-      <VictoryChart width={350} height={250}>
-        <VictoryAxis
-          style={{
-            axis: { stroke: "transparent" },
-            ticks: { stroke: "transparent" },
-            tickLabels: { fontSize: 12 },
-          }}
-        />
-
-        <VictoryBar
-          barWidth={30}
-          cornerRadius={{ top: 8, bottom: 8 }}
-          data={data}
-          x="month"
-          y="amount"
-          events={[
-            {
-              target: "data",
-              eventHandlers: {
-                onPressIn: () => setNum((prev) => prev + 1),
-              },
-            },
-          ]}
-        />
-      </VictoryChart>
+      <DropDownPicker
+        placeholder={`${value}월`}
+        open={openDropDown}
+        setOpen={setOpenDropDown}
+        value={value}
+        setValue={setValue}
+        items={item}
+        setItems={setItem}
+      />
+      <ChartContainer>
+        <MonthConSimption>
+          {value.split(" ")[0]}의 소비금액은 {value.split(" ")[1]}원
+        </MonthConSimption>
+        <AntDesign name="caretdown" />
+        <VictoryChart width={350} height={250}>
+          <VictoryAxis
+            style={{
+              axis: { stroke: "transparent" },
+              ticks: { stroke: "transparent" },
+              tickLabels: { fontSize: 12 },
+            }}
+          />
+          <VictoryBar
+            barWidth={30}
+            cornerRadius={{ top: 8, bottom: 8 }}
+            data={data}
+            x="month"
+            y="amount"
+          />
+        </VictoryChart>
+      </ChartContainer>
     </Container>
   );
 };
