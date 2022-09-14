cb2527a9635a162cc171aca83f0ee87997e8d930
hyeonmi3 <cc146@daum.net>
2022-09-14 18:06:53 +0900

    auth screen css

--- src/screens/Auth/Auth.jsx
+++ src/screens/Auth/Auth.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { TouchableOpacity, Text, StyleSheet, View, Image } from "react-native";
 import AuthImage from '../Images/AuthImage.png';
+import GoogleImage from '../Images/Google.png';
 
 const styles = StyleSheet.create({
   all: {
@@ -8,7 +9,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   loginButton: {
-    width: 303,
+    width: 280,
     height: 64,
     backgroundColor: '#538EE6',
     borderRadius: 15,
@@ -16,7 +17,7 @@ const styles = StyleSheet.create({
     alignItems: "center"
   },
   signupButton: {
-    width: 303,
+    width: 280,
     height: 64,
     backgroundColor: '#F08D8D',
     borderRadius: 15,
@@ -29,24 +30,32 @@ const styles = StyleSheet.create({
     marginTop: 30,
     marginBottom: 42,
   },
-  login_signup_text: {
-    marginTop: 20,
-    marginBottom: 40,
+  login_signup_button: {
+    marginTop: 12,
+    marginBottom: 60,
   },
   login_signup_text_size: {
     fontSize: 20,
     color: "#fff"
   },
   main_text: {
-    marginBottom: 40,
+    marginBottom: 38,
     alignItems: 'center',
     justifyContent: 'center'
   },
   main_text_size: {
-    fontSize: 28
+    fontSize: 27,
+  },
+  last_text: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  google_image: {
+    marginRight: 7
   },
   last_text_size: {
-    fontSize: 16
+    fontSize: 16,
+    color: "#A1A1A1"
   },
 });
 
@@ -68,14 +77,17 @@ const Auth = ({ navigation }) => {
         </TouchableOpacity>
       </View>
 
-      <View style={styles.login_signup_text}>
+      <View style={styles.login_signup_button}>
         <TouchableOpacity activeOpacity={0.8} style={styles.signupButton} onPress={() => {
             navigation.navigate("SignIn");}}>
           <Text style={styles.login_signup_text_size}>회원가입</Text>
         </TouchableOpacity>
       </View>
 
-      <Text style={styles.last_text_size}>구글 계정으로 로그인 / 회원가입 됩니다.</Text>
+      <View style={styles.last_text}>
+        <Image style={styles.google_image} source={GoogleImage}/>
+        <Text style={styles.last_text_size}>구글 계정으로 로그인 / 회원가입 됩니다.</Text>
+      </View>
     </View>
   );
 };
