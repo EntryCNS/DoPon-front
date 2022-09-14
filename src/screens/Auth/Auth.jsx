6109986a47d3fab0e069e64cadf63a34fe2a373d
hyeonmi3 <cc146@daum.net>
2022-09-14 17:42:27 +0900

    auth screen css

--- src/screens/Auth/Auth.jsx
+++ src/screens/Auth/Auth.jsx
@@ -23,25 +23,59 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center"
   },
-  text: {
+  main_image: {
+    width: 320,
+    height: 239,
+    marginTop: 30,
+    marginBottom: 42,
+  },
+  login_signup_text: {
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  login_signup_text_size: {
+    fontSize: 20,
     color: "#fff"
-  }
+  },
+  main_text: {
+    marginBottom: 40,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  main_text_size: {
+    fontSize: 28
+  },
+  last_text_size: {
+    fontSize: 16
+  },
 });
 
 const Auth = ({ navigation }) => {
   console.log(navigation);
   return (
     <View style={styles.all}>
-      <Image source={AuthImage}/>
-      <Text>당신의 소비생활은{'\n'}건강하신가요?</Text>
-      <TouchableOpacity activeOpacity={0.8} style={styles.loginButton} onPress={() => {
-          navigation.navigate("SignIn");}}>
-        <Text style={styles.text}>로그인</Text>
-      </TouchableOpacity>
-      <TouchableOpacity activeOpacity={0.8} style={styles.signupButton} onPress={() => {
-          navigation.navigate("SignIn");}}>
-        <Text style={styles.text}>회원가입</Text>
-      </TouchableOpacity>
+      <Image style={styles.main_image} source={AuthImage}/>
+
+      <View style={styles.main_text}>
+        <Text style={styles.main_text_size}>당신의 소비생활은</Text>
+        <Text style={styles.main_text_size}>건강하신가요?</Text>
+      </View>
+
+      <View>
+        <TouchableOpacity activeOpacity={0.8} style={styles.loginButton} onPress={() => {
+            navigation.navigate("SignIn");}}>
+          <Text style={styles.login_signup_text_size}>로그인</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.login_signup_text}>
+        <TouchableOpacity activeOpacity={0.8} style={styles.signupButton} onPress={() => {
+            navigation.navigate("SignIn");}}>
+          <Text style={styles.login_signup_text_size}>회원가입</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.last_text_size}>구글 계정으로 로그인 / 회원가입 됩니다.</Text>
     </View>
   );
 };
